ce38e46f041ade8bbd2c7d04d7cdc103a939e277
@147403 <seankenny.kenny@strathmore.edu>
2024-07-11 13:40:01 +0300

    index.php modified

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 
 
-<   
+
 <?php include_once("templates/test.php")?>
 </style>   
   <style>
@@ -40,7 +40,7 @@
 
         <section id="about">
     
-            <p> where we provide a holistic, individualized, and outstanding education utilizing learning standards
+            <p>To provide a holistic, individualized, and outstanding education utilizing learning standards
                 based on American curriculum. Our multicultural environment and educational philosophy
                 foster communication, critical thinking, leadership skills, digital literacy and exemplary
                 character, in order to prepare students for their future pursuits.
